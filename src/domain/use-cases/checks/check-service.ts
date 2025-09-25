a58bf62e0c8d654d43a5bd6575af679a1d95c2a5
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-25 18:34:39 +0200

    creación del Repository pattern

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase{
     execute(url:string):Promise<boolean>;
 }
@@ -8,6 +11,7 @@ type ErrorCallback=(error:string)=>void;
 export class CheckService implements CheckServiceUseCase{
 
     constructor(
+        private readonly logRepository:LogRepository,
         private readonly successCallback:SuccessCallback,
         private readonly errorCallback:ErrorCallback
     ){}
@@ -21,13 +25,19 @@ export class CheckService implements CheckServiceUseCase{
                 throw new Error(`Error on check service ${url}`);
             }
 
+            const log=new LogEntity(`Service ${url} working`,LogSeverityLevel.low);
+            this.logRepository.saveLog(log);
             this.successCallback();
             //console.log(`${url} is ok`);
             return true;
 
         } catch (error) {
-            this.errorCallback(`${error}`);
-            console.log(`${error}`);
+            const errorMessage=`${url} is not ok. ${error}`;
+            const log=new LogEntity(errorMessage,LogSeverityLevel.high);
+            this.logRepository.saveLog(log);
+
+            this.errorCallback(errorMessage);
+            //console.log(`${error}`);
             return false;
         }
     }
