a7e2fe848f32739041a61f739b5be350632109d0
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-27 20:39:24 +0200

    pruebas en el directorio mongo

---
+++ src/data/mongo/init.test.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { MongoDatabase } from "./init";
+
+describe('init MongoDB',()=>{
+
+    afterAll(()=>{
+        mongoose.connection.close();
+    });
+
+    test('should connect to MongoDB',async()=>{
+        const connected=await MongoDatabase.connect({
+            dbName:process.env.MONGO_DB_NAME!,
+            mongoUrl:process.env.MONGO_URL!
+        });
+        expect(connected).toBe(true);
+    });
+
+    test('should throw an error',async()=>{
+        try {
+            const connected=await MongoDatabase.connect({
+                dbName:process.env.MONGO_DB_NAME!,
+                mongoUrl:'mongodb://irene:123456@looooocalhost:27017'
+            });
+            expect(true).toBe(false);
+        } catch (error) {
+            
+        }
+
+
+        //expect(connected).toBe(true);
+    });
+});
