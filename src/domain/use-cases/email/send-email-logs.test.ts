bae167bc38917f903a0ca2f7d4ab865f22c4a9e3
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-28 09:55:46 +0200

    pruebas en SendEmailLogs UseCase

---
+++ src/domain/use-cases/email/send-email-logs.test.ts
@@ -0,0 +1,61 @@
+import { EmailService } from "../../../presentation/email/email.service";
+import { LogEntity } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+import { SendEmailLogs } from "./send-email-logs";
+
+describe('send-email-logs.ts',()=>{
+
+    const mockEmailService={
+        sendEmailWithFileSystemLogs:jest.fn().mockReturnValue(true)
+    }
+
+    const mockLogRepository:LogRepository={
+        saveLog:jest.fn(),
+        getLogs:jest.fn()
+    }
+
+    const sendEmailLogs=new SendEmailLogs(
+        mockEmailService as any,
+        mockLogRepository
+    );
+
+    beforeEach(()=>{
+        jest.clearAllMocks();
+    });
+
+    test('should call sendEmail and saveLog',async()=>{
+
+        const result=await sendEmailLogs.execute('ireneog_72@hotmail.es');
+
+        expect(result).toBe(true);
+        expect(mockEmailService.sendEmailWithFileSystemLogs).toHaveBeenCalledTimes(1);
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith(expect.any(LogEntity));
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith({
+            createdAt: expect.any(Date),
+            level: "low",
+            message: "Log email sent",
+            origin: "send-email-logs.ts",
+        });
+
+    });
+
+    test('should log in case of error',async()=>{
+
+        mockEmailService.sendEmailWithFileSystemLogs.mockResolvedValue(false);
+
+        const result=await sendEmailLogs.execute('ireneog_72@hotmail.es');
+
+        //expect(result).toBe(false);
+        expect(mockEmailService.sendEmailWithFileSystemLogs).toHaveBeenCalledTimes(1);
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith(expect.any(LogEntity));
+        /*
+        expect(mockLogRepository.saveLog).toHaveBeenCalledWith({
+            createdAt: expect.any(Date),
+            level: "low",
+            message: "Error: Email log not sent",
+            origin: "send-email-logs.ts",
+        });
+        */
+    });
+
+});
